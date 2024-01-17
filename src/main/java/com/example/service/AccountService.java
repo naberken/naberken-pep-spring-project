ba3416c34ee180b4cc63b2f40097beff482cda1a
Nick Berken <berkenn8020@uwstout.edu>
2024-01-17 05:24:16 +0000

    started

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,33 @@
 package com.example.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
+@Transactional
 public class AccountService {
+
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerAccount(Account account){
+        if (account.getPassword().length() <= 4){
+            return null;
+        }
+
+        return accountRepository.save(account);
+    }
+
+    public Account loginAccount(Account account){
+        return accountRepository.findAccountByUsernameAndPassword(account.getUsername(), account.getPassword());
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,49 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
+@Transactional
 public class MessageService {
+
+    MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
+
+    public Message postMessage(Message message){
+        return messageRepository.save(message);
+    }
+
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
+
+    public Message getMessageById(long message_id){
+        return messageRepository.getById(message_id);
+    }
+
+    public void deleteMessageById(long delete_id){
+        messageRepository.deleteById(delete_id);
+    }
+
+    public Message patchMessage(String message_text, long patch_id){
+        Message message = messageRepository.findById(patch_id).get();
+        message.setMessage_text(message_text);
+        messageRepository.save(message);
+        return message;
+    }
+
+    public List<Message> getMessagesByAccount(int posted_by){
+        return messageRepository.findMessagesByPostedBy(posted_by);
+    }
 }
