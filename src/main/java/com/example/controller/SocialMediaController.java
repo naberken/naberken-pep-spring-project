16d1770328e754f6bb1dd6266c29be1e257919bb
Nick Berken <berkenn8020@uwstout.edu>
2024-01-17 06:17:49 +0000

    started 2

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,19 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +21,47 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    
+    @Autowired
+    AccountService accountService;
+    
+    @Autowired
+    MessageService messageService;
 
+    @PostMapping("/register")
+    public Account registerAccount(@RequestBody Account account){
+        return accountService.registerAccount(account);
+    }
+
+    @PostMapping("/login")
+    public Account loginAccount(@RequestBody Account account){
+        return accountService.loginAccount(account);
+    }
+
+    @PostMapping("/messages")
+    public Message postMessage(@RequestBody Message message){
+        return messageService.postMessage(message);
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getAllMessages(){
+        return messageService.getAllMessages();
+    }
+
+    @GetMapping("/messages/{message_id}")
+    public Message getMessageById(@PathVariable long message_id){
+        return messageService.getMessageById(message_id);
+    }
+
+    @DeleteMapping("/messages/{message_id}")
+    public void deleteMessageById(@PathVariable long message_id){
+        messageService.deleteMessageById(message_id);
+    }
+
+    @GetMapping("/accounts/{account_id}/messages")
+    public List<Message> getMessagesByAccountId(@PathVariable int account_id){
+        return messageService.getMessagesByAccount(account_id);
+    }
 }
